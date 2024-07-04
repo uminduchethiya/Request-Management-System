19fa55429255abf0ce299322616db7399f1f6aff
Umindu Chethiya <122140505+uminduchethiya@users.noreply.github.com>
2024-07-04 08:43:12 +0530

    front end done with out pagination

--- frontend/src/components/AdminLayout.jsx
+++ frontend/src/components/AdminLayout.jsx
@@ -1,18 +1,20 @@
 // AdminLayout.jsx
 
-import Header from "./header";
+
+import Header from "./header"; // Assuming correct import path
+import "./AdminLayout.css"; // Import your component-specific stylesheet
 
 const AdminLayout = ({ children }) => {
     return (
-        <div className=" flex flex-col h-screen w-full ">
-          <Header />
-          <main className="flex-1 overflow-y-auto bg-gray-200 w-full ">
-            <div className=" mt-20 border ">
-              {children}
-            </div>
-          </main>
+        <div className="flex flex-col h-screen w-full font-inter">
+            <Header />
+            <main className="flex-1 overflow-y-auto bg-gray-200 w-full">
+                <div className="mt-20 border">
+                    {children}
+                </div>
+            </main>
         </div>
-      );
-    };
+    );
+};
 
 export default AdminLayout;
