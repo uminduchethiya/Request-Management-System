ed127f95053f8fb4faed9aba94be826137e0efdd
Umindu Chethiya <122140505+uminduchethiya@users.noreply.github.com>
2024-07-04 10:48:34 +0530

    login and register ui done

--- frontend/src/views/auth/Signup.jsx
+++ frontend/src/views/auth/Signup.jsx
@@ -37,8 +37,8 @@ export default function Signup() {
     <div className="login-signup-form animated fadeInDown">
       <section className="bg-gray-50 dark:bg-gray-900">
         <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
-          <a href="#" className="flex items-center mb-6 text-2xl font-semibold text-gray-900 dark:text-white">
-            LARAVEL REACT
+          <a href="#" className="flex items-center mb-6 text-2xl  text-black font-bold dark:text-white">
+            E-HOSPITAL
           </a>
           <div className="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
             <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
@@ -107,11 +107,11 @@ export default function Signup() {
 
                 <button
                   type="submit"
-                  className="w-full text-black bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800"
+                  className="w-full text-white bg-[#830823] hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800"
                 >
                   Create an account
                 </button>
-                <p className="text-sm font-light text-gray-500 dark:text-gray-400">
+                <p className="text-sm font-light  text-gray-500 dark:text-gray-400">
                   Already have an account?{' '}
                   <a href="/signin" className="font-medium text-primary-600 hover:underline dark:text-primary-500">
                     Login here
